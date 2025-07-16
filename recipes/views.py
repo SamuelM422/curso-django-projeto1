e9f329ac1f64342d86b652fb101a5fdc13533230
Samuel Silva <minechannelbrasil@gmail.com>
2025-07-16 20:14:26 -0300

    created recipes and populated with fake data

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
     return render(request, "recipes/pages/home.html", context={
-        'name': 'Samuel'
+        'recipes': [make_recipe() for _ in range(12)]
+    })
+
+def recipes(request, id):
+    return render(request, "recipes/pages/recipe-view.html", context={
+        'recipe': make_recipe(),
+        'is_detail_page': True
     })
