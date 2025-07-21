d32166c6d896068e0a45faf94b0c545c92173a2a
Samuel Silva <samuel.m.silva0422@gmail.com>
2025-07-21 15:50:31 -0300

    added authors app with registration form, URLs, and views; updated project URLs and settings for authors app integration; and added `type: ignore` comments to static file serving configuration
    Signed-off-by: Samuel Silva <samuel.m.silva0422@gmail.com>

--- project/urls.py
+++ project/urls.py
@@ -11,8 +11,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urlpatterns import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urlpatterns'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -21,8 +21,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
+    path('authors/', include('authors.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # type: ignore
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # type: ignore
