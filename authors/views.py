6670cb0fe6a19f55d8beea5cd716b0146fe2fb4f
Samuel Silva <samuel.m.silva0422@gmail.com>
2025-07-21 17:55:22 -0300

    added authors app with initial setup: models, views, templates, tests, and configuration
    Signed-off-by: Samuel Silva <samuel.m.silva0422@gmail.com>

---
+++ authors/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from authors.forms import RegisterForm
+from django.http import Http404 # type: ignore
+
+
+# Create your views here.
+def register_view(request):
+    register_form_data = request.session.get('register_form_data')
+    form = RegisterForm(register_form_data)
+    return render(request, 'authors/pages/register_view.html',
+                  context={'form': form}
+                  )
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    request.session['register_form_data'] = request.POST
+    form = RegisterForm(request.POST)
+
+    return redirect('authors:register')
