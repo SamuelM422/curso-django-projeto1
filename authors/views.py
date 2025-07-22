a948c259c18ff9f5288e5264d1a48079f8c9ca48
Samuel Silva <samuel.m.silva0422@gmail.com>
2025-07-22 15:23:51 -0300

    refactored templates and static files to use global directories, updated paths across templates and views to ensure consistency and reusability
    Signed-off-by: Samuel Silva <samuel.m.silva0422@gmail.com>

--- authors/views.py
+++ authors/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from authors.forms import RegisterForm
 from django.http import Http404 # type: ignore
 from django.contrib import messages
+from django.urls import reverse
 
 # Create your views here.
 def register_view(request):
     register_form_data = request.session.get('register_form_data')
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html',
-                  context={'form': form}
+                  context={'form': form,
+                           'form_action': reverse('authors:create')}
                   )
 
 def register_create(request):
