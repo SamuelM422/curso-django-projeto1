2c2c8a305c2bcdc74e66207dfc100124c553d1ba
Samuel Silva <samuel.m.silva0422@gmail.com>
2025-07-22 14:44:15 -0300

    updated registration view to hash passwords before saving users, ensuring security
    Signed-off-by: Samuel Silva <samuel.m.silva0422@gmail.com>

--- authors/views.py
+++ authors/views.py
@@ -19,7 +19,9 @@ def register_create(request):
     form = RegisterForm(request.POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Account created successfully')
 
         del(request.session['register_form_data'])
