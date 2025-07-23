8e08f25be6d7e512b32e58dcac2351544109d7a4
Samuel Silva <samuel.m.silva0422@gmail.com>
2025-07-23 15:23:04 -0300

    added functional tests for authors' login edge cases, including invalid credentials, invalid form submissions, and 404 response on non-POST login attempt
    Signed-off-by: Samuel Silva <samuel.m.silva0422@gmail.com>

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -1,5 +1,4 @@
 from selenium.webdriver import Keys
-
 from tests.functional_tests.authors.base import AuthorsBaseTest
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -24,12 +23,53 @@ class AuthorsLoginTest(AuthorsBaseTest):
         username_field = self.get_by_placeholder(form, 'Type your username here')
         password_field = self.get_by_placeholder(form, 'Your password here')
 
-        username_field.send_keys('my_user')
+        username_field.send_keys(user.username)
         password_field.send_keys('my_password')
 
         form.submit()
 
         self.assertIn(
             'You are logged in with my_user.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(self.live_server_url + reverse('authors:login_create'))
+
+        self.assertIn('Not Found', self.browser.find_element(By.TAG_NAME, 'body').text)
+
+    def test_form_login_is_invalid(self):
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        username_field = self.get_by_placeholder(form, 'Type your username here')
+        password_field = self.get_by_placeholder(form, 'Your password here')
+
+        username_field.send_keys(' ')
+        password_field.send_keys(' ')
+
+        form.submit()
+
+        self.assertIn(
+            'Invalid username or password.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+    def test_form_login_with_invalid_credentials(self):
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        username_field = self.get_by_placeholder(form, 'Type your username here')
+        password_field = self.get_by_placeholder(form, 'Your password here')
+
+        username_field.send_keys('invalid_username')
+        password_field.send_keys('invalid_password')
+
+        form.submit()
+
+        self.assertIn(
+            'Invalid credentials.',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
