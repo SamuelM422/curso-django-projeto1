b83077301b6d8931a65d2f22fb5264a9e18ff4ef
Samuel Silva <samuel.m.silva0422@gmail.com>
2025-07-28 07:34:33 -0300

    updated project for production readiness: switched to environment-based database configuration, expanded `.env-example` for PostgreSQL support, set `ALLOWED_HOSTS` to wildcard, adjusted IDE settings, and added pytest-dotenv to requirements
    Signed-off-by: Samuel Silva <samuel.m.silva0422@gmail.com>

--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -27,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = []  # type: ignore
+ALLOWED_HOSTS = ['*']  # type: ignore
 
 # Application definition
 
@@ -79,8 +78,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DATABASE_ENGINE'),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
